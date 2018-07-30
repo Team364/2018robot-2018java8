7f1c2955db1db4340069ec380ab7a37fd94100d6
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2018-07-30 12:12:06 -0500

    Button Changes(cont.)
    Fixed signs on trigger axis

--- src/main/java/frc/team364/robot/commands/teleop/TeleopIntakeCommand.java
+++ src/main/java/frc/team364/robot/commands/teleop/TeleopIntakeCommand.java
@@ -21,10 +21,10 @@ public class TeleopIntakeCommand extends Command {
        
         // Run the intake/outtake on button press.
         // No toggle.
-        if(Robot.oi.controller.getRawAxis(2) < 0.5) {
+        if(Robot.oi.controller.getRawAxis(2) > 0.5) {
             Robot.intakeSystem.intake();
             System.out.println("Intake is running");
-        } else if(Robot.oi.controller.getRawAxis(3) < 0.5) {
+        } else if(Robot.oi.controller.getRawAxis(3) > 0.5) {
             Robot.intakeSystem.outtake();
             System.out.println("Outtake is running");
         } else {
