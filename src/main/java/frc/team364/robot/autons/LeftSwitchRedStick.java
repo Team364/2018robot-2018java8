256c284f0191d9e8b5a0ddeb03e94200f4317268
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2018-08-06 13:09:30 -0500

    Tweeked LeftSwitchRedStick(cont)

--- src/main/java/frc/team364/robot/autons/LeftSwitchRedStick.java
+++ src/main/java/frc/team364/robot/autons/LeftSwitchRedStick.java
@@ -10,23 +10,23 @@ import frc.team364.robot.commands.auto.misc.*;
 public class LeftSwitchRedStick extends CommandGroup {
 
     public LeftSwitchRedStick() {
-       addSequential(new DriveStraightForCounts(1500, false, false));
-       addSequential(new TurnToHeading(-22));
-       addSequential(new DriveStraightForCounts(5000, false, false));
-       addSequential(new TurnToHeading(20));
+       addSequential(new DriveStraightForCounts(1500, false, false));//Move away from wall
+       addSequential(new TurnToHeading(-26));//Turn left
+       addSequential(new DriveStraightForCounts(5000, false, false));//Drive forward
+       addSequential(new TurnToHeading(20));//Turn towards switch
        addParallel(new LiftSecondStage());
-       addSequential(new DriveStraightForCounts(2500, false, false));
+       addSequential(new DriveStraightForCounts(2800, false, false));//Drive to switch
        addSequential(new FlipClawDown());
        addSequential(new OuttakeCube());
        addParallel(new DropSecondStage());
-       addSequential(new DriveStraightForCounts(1500, true, false));
+       addSequential(new DriveStraightForCounts(1500, true, false));//Back up
        //SecondCube
-       addSequential(new TurnToHeading(37));
-       addSequential(new DriveStraightForCountsIntake(3000, false, false));
+       addSequential(new TurnToHeading(37));//Turn towards second cube
+       addSequential(new DriveStraightForCountsIntake(3000, false, false));//Drive into cube
        addSequential(new WaitCommand(0.2));
-       addSequential(new ShortIntakeCube());
-       addSequential(new DriveStraightForCountsIntake(2000, true, false));
-       addSequential(new TurnToHeading(-33));
+       addSequential(new ShortIntakeCube());//Intake cube
+       addSequential(new DriveStraightForCountsIntake(2000, true, false));//Back up from Cube Stack
+       addSequential(new TurnToHeading(-33));//Turn Towards switch
        addSequential(new ResetEncoders());
        addParallel(new LiftSecondStageHalfway());
        addSequential(new DriveStraightForCounts(2000, false, false));
