8aa3f61b5bd1cdabed47fd4ca231e13d04920768
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2018-08-07 12:24:05 -0500

    Comments
    shows which drive motors have encoders

--- src/main/java/frc/team364/robot/RobotMap.java
+++ src/main/java/frc/team364/robot/RobotMap.java
@@ -4,8 +4,14 @@ public class RobotMap {
 
     // Drive objects
     public static final int leftFrontDrive = 14;
+    /**
+     * has encoder
+     */
     public static final int leftRearDrive = 15;
     public static final int rightFrontDrive = 10;
+    /**
+     * has encoder
+     */
     public static final int rightRearDrive = 11;
     public static final int shifterPort1 = 3;
     public static final int shifterPort2 = 4;
