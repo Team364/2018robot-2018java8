651f8884476b56aa99711d6719a5793249603194
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2018-08-27 15:30:12 -0500

    Testing
    Got rid of more stuff unrelated to drive
    base

--- src/main/java/frc/team364/robot/RobotMap.java
+++ src/main/java/frc/team364/robot/RobotMap.java
@@ -16,29 +16,4 @@ public class RobotMap {
     public static final int shifterPort1 = 3;
     public static final int shifterPort2 = 4;
 
-    // First stage elevator objects
-    public static final int firstStage1 = 8;//was 3
-    public static final int firstStage2 = 13;
-    public static final int firstStageBottomLimit = 1;
-    public static final int firstStageTopLimit = 0;
-
-    // Second stage elevator objects
-    public static final int secondStage1 = 5;
-    public static final int secondStage2 = 4;
-    public static final int secondStageBottomLimit = 2;
-    public static final int secondStageTopLimit = 3;
-
-    // Intake objects
-    public static final int intakeLeft = 1;
-    public static final int intakeRight = 2;
-
-    // Intake/Claw objects
-    public static final int clawPistonPort1 = 0;//formerly 1--having electrical problems so Flipping claw and buddy
-    public static final int clawPistonPort2 = 7;//formerly 6
-    public static final int pinchPistonPort1 = 2;
-    public static final int pinchPistonPort2 = 5;
-
-    // Buddy bar objects
-    public static final int buddyBarPistonPort1 = 1;//formerly 0
-    public static final int buddyBarPistonPort2 = 6;//formerly 7
 }
